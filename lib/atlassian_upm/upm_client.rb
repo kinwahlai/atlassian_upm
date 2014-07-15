52f59f39b155c03b6bdb64c01b7768f884479f3d
kinwah <kinwah.lai@gmail.com>
2014-07-15 11:32:41 +0800

    remove duplicates in test

--- lib/atlassian_upm/upm_client.rb
+++ lib/atlassian_upm/upm_client.rb
@@ -9,4 +9,8 @@ class UPMClient
   def token
     @rest_client.head.headers[:upm_token]
   end
+
+  def uploadPlugin(filename)
+    @rest_client.post({:plugin => File.new(filename), :multipart => true}, {:token => token()})
+  end
 end
--- spec/upm_spec.rb
+++ spec/upm_spec.rb
@@ -3,8 +3,19 @@ require 'webmock/rspec'
 
 describe "Atlassian Universal Plugin Manager" do
 
-  it "should be get the upm_token" do
+  before (:each) do
+    @client = UPMClient.new('url','user','password')
     stub_request(:head, "user:password@url").to_return(:headers => { 'upm_token' => 'token123' })
-    UPMClient.new('url','user','password').token.should == 'token123'
+    stub_request(:post, "user:password@url").to_return(:status => 200)
+  end
+
+  it "should be get the upm_token" do
+    expect(@client.token).to eq 'token123'
+  end
+
+  it "should be able to post the plugin to host" do
+    expect(File).to receive(:new).with("filename").and_return(double)
+    @client.uploadPlugin("filename")
+    expect(WebMock).to have_requested(:post, "user:password@url").with(:headers => {'token' => 'token123', 'Content-Type'=> /multipart\/form-data\; boundary=[\d]+/})
   end
 end
